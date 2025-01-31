e59c08bce41f38d709165c522179d198ddce1596
ATHERLY-Evan-24012805 <evan.atherly@etu.univ-amuf.fr>
2025-01-31 15:15:20 +0000

    modification du fichier

--- src/exercice1/Hello.java
+++ src/exercice1/Hello.java
@@ -2,6 +2,7 @@ package exercice1;
 
 public class Hello {
 	public static void main(String[] args){
-		System.out.println("Hello word");
+		System.out.println("Hello world");
+		System.out.println("hola");
 	}
 }
